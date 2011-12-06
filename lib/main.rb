d183df1143da7232de5181a5fa73e686df66bf4c
Joas1988 <Joas1988@gmail.com>
2011-12-06 03:17:12 +0100

    Update lib/main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -34,3 +34,26 @@ all_ep_forms(agent, "29.10.2011", "03.05.2012", ti) do |id, page|
   skills_in_table = form.serialize(skills_in_table, connection)
   puts "EP form extracted (#{ti.stop('db')})"
 end
+
+
+=begin
+my_file = File.new("db.txt","w")
+all_ep_forms(agent, "5.12.2007", "30.09.2012", ti) do |id, page|
+preskoc=false
+a=id.to_s
+page.parser.xpath(('/html/body/form/table/tr/td/table/tr/td/div/div/table/tr/td')).each do |node|
+    if ((node['class']!="td-grayclass-leftalign" and not((node.text).to_s.include? "Status") and not((node.text).to_s.include? "Exchange Type")  ) or (node.text).to_s.include? "Native" or (node.text).to_s.include? "Excellent" or (node.text).to_s.include? "Good" or (node.text).to_s.include? "Basic")
+        then
+        if (((node.text).to_s.delete "\t\r\n").include? "ProfessionalInformation" or ((node.text).to_s.delete "\t\r\n").include? "AIESECInformation") then  preskoc=true end
+        if (((node.text).to_s.delete "\t\r\n").include? "PersonalPreferences" or ((node.text).to_s.delete "\t\r\n").include? "Backgrounds") then preskoc=false end
+        if (node['colspan']!="2" and preskoc==false)
+        then
+        a+=';'+(((node.text).to_s).delete "\t\r\n")
+        end
+        end
+end
+my_file.puts a
+  
+  puts "EP form extracted (#{ti.stop('db')})"
+end
+=end
